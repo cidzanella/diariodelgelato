624a03d8f3f0d769edafe201fe888c6e5398a2a2
cidzanella <cidzanella@gmail.com>
2023-08-22 09:14:29 -0300

    adjusting branchs

---
+++ Backend/DiarioDelGelato/Src/DiarioDelGelato.Infrastructure/Identity/models/Authentication/TokenResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiarioDelGelato.Infrastructure.Identity.Models.Authentication
+{
+    internal class TokenResponse
+    {
+    }
+}
